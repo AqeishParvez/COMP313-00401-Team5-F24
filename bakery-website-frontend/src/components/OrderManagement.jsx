40bd6c6a8f6b795e2250928409ad8c8489335070
wilkie <feanor4d@gmail.com>
2024-12-02 11:07:38 -0500

    3 Completed - Sort / filter for Order History / Dashboard
    refactor the orderTable to prevent error.
    customer and staff will now see 2 'different' order board

--- bakery-website-frontend/src/components/OrderManagement.jsx
+++ bakery-website-frontend/src/components/OrderManagement.jsx
@@ -3,11 +3,11 @@ import axios from 'axios';
 import { Table, Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
-import { useTable, useSortBy } from 'react-table'; 
+import { useTable, useSortBy, useFilters, useGlobalFilter } from 'react-table'; 
 
 
 const OrderManagement = () => {
-  const { getAuthHeader } = useAuth();
+  const { getAuthHeader, userRole } = useAuth();
   const [orders, setOrders] = useState([]);
   const [staffList, setStaffList] = useState([]);
   const [productSummary, setProductSummary] = useState([]); // To store aggregated product summary
@@ -84,23 +84,116 @@ const OrderManagement = () => {
     }
   };
 
-  const columns = React.useMemo(
+  // Filter by input
+  const ColumnFilter = ({ column: { filterValue, setFilter, id, getSortByToggleProps } }) => {
+    const stopPropagation = (e) => {
+      e.stopPropagation();
+    };
+  
+    return (
+      // warp this in another div to stop 'sorting' with click to filter
+      <div {...getSortByToggleProps()} onClick={stopPropagation}>
+        <input
+          value={filterValue || ''}
+          onChange={e => setFilter(e.target.value || undefined)} // Update the filter value
+          placeholder={`Search ${id}`}
+          style={{ width: '100%' }}
+        />
+      </div>
+    );
+  };
+
+  // Filter by existing option
+  const ColumnDropdownFilter = ({ column: { filterValue, setFilter, preFilteredRows, id, getSortByToggleProps } }) => {
+    const options = React.useMemo(() => {
+      // Go thru the row to get all options and converte them to unique 'id'
+      const values = new Set();
+      preFilteredRows.forEach(row => {
+        values.add(row.values[id]);
+      });
+      return [...values];
+    }, [id, preFilteredRows]);
+
+    const handleDropdownChange = (e) => {
+      setFilter(e.target.value || undefined);
+    };
+
+    // Because no one like junky sorting (prevent sort when click on filter)
+    const stopPropagation = (e) => {
+      e.stopPropagation();
+    };
+
+    // actual part to filter, see similarity to ColumnFilter
+    return (
+      <div {...getSortByToggleProps()} onClick={stopPropagation}>
+        <select
+          value={filterValue || ''}
+          onChange={handleDropdownChange}
+          style={{ width: '100%' }}>
+          <option value="">All</option>
+          {options.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  };
+
+  const staffColumns = React.useMemo(
     () => [
       { Header: 'Customer',
-        accessor: 'customer.name',},
+        accessor: 'customer.name',
+        Filter: ColumnFilter,},
       { Header: 'Status',
-        accessor: 'status',},
+        accessor: 'status',
+        Filter: ColumnDropdownFilter,},
       {  Header: 'Priority',
-        accessor: 'priority',},
+        accessor: 'priority',
+        Filter: ColumnDropdownFilter,},
       { Header: 'Products',
         accessor: 'products',
+        Filter: () => null,
         Cell: ({ row }) => row.original.products.map(product => `${product.product.name} (x${product.quantity})`).join(', '),},
       { Header: 'Assigned Staff',
-        accessor: 'assignedStaff.name',},
+        accessor: 'assignedStaff.name',
+        Filter: ColumnDropdownFilter,},
       { Header: 'Total Price',
         accessor: 'totalPrice',
+        Filter: () => null,
         Cell: ({ value }) => value.toFixed(2),},
       { Header: 'Actions',
+        Filter: () => null,
+        Cell: ({ row }) => (
+          <>
+            <Button variant="info" onClick={() => handleViewOrder(row.original._id)} className="me-2">View</Button>
+            <Button variant="warning" onClick={() => handleEditOrder(row.original._id)}>Edit</Button>
+            <Button variant="danger" onClick={() => handleDeleteOrder(row.original._id)}>Cancel Order</Button>
+          </>
+        ),},
+    ],
+    []
+  );
+  
+  const customerColumns = React.useMemo(
+    () => [
+      { Header: 'Status',
+        accessor: 'status',
+        Filter: ColumnDropdownFilter,},
+      { Header: 'Products',
+        accessor: 'products',
+        Filter: () => null,
+        Cell: ({ row }) => row.original.products.map(product => `${product.product.name} (x${product.quantity})`).join(', '),},
+      { Header: 'Assigned Staff',
+        accessor: 'assignedStaff.name',
+        Filter: ColumnDropdownFilter,},
+      { Header: 'Total Price',
+        accessor: 'totalPrice',
+        Filter: () => null,
+        Cell: ({ value }) => value.toFixed(2),},
+      { Header: 'Actions',
+        Filter: () => null,
         Cell: ({ row }) => (
           <>
             <Button variant="info" onClick={() => handleViewOrder(row.original._id)} className="me-2">View</Button>
@@ -112,12 +205,15 @@ const OrderManagement = () => {
     []
   );
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, state: { sortBy } } = useTable(
+  const columns = userRole !== 'customer' ? staffColumns : customerColumns;
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, setFilter, state: { sortBy } } = useTable(
     { columns, data: orders },
+    useFilters,
     useSortBy
   );
 
-  
+
   return (
     <div>
       <h3>Manage Orders</h3>
@@ -147,35 +243,42 @@ const OrderManagement = () => {
       <h4>Orders List</h4>
       <Table striped bordered hover className="mt-4" {...getTableProps()}>
       <thead>
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                  {column.render('Header')}
-                  <span>
-                    {column.isSorted
-                      ? column.isSortedDesc
-                        ? ' ↓'
-                        : ' ↑'
-                      : ''}
-                  </span>
-                </th>
+        {headerGroups.map(headerGroup => {
+          const headerGroupProps = headerGroup.getHeaderGroupProps();
+          return (
+            <tr {...headerGroupProps} key={headerGroupProps.key}>
+              {headerGroup.headers.map(column => {
+                const { key, ...columnProps } = column.getHeaderProps(column.getSortByToggleProps());
+                return (
+                  <th key={key} {...columnProps}>
+                    {column.render('Header')}
+                    <span>
+                      {column.isSorted
+                        ? column.isSortedDesc
+                          ? ' ↓'
+                          : ' ↑'
+                        : ''}
+                    </span>
+                    <div>{column.render('Filter')}</div>
+                  </th>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map(row => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()} key={row.id}>
+              {row.cells.map(cell => (
+                <td {...cell.getCellProps()} key={`${row.id}-${cell.column.id}`}>{cell.render('Cell')}</td>
               ))}
             </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map(row => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
+          );
+        })}
+      </tbody>
       </Table>
     </div>
   );
